e9a041f139b722c98544a6da83f64fd9945338e9
Aldo Fuentes <35090917+Aldoukfm@users.noreply.github.com>
2019-02-17 21:57:19 -0600

    Initial Commit

---
+++ GQLControl/GQLControl.h
@@ -0,0 +1,19 @@
+//
+//  GQLControl.h
+//  GQLControl
+//
+//  Created by Aldo Fuentes on 2/17/19.
+//  Copyright © 2019 aldofuentes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GQLControl.
+FOUNDATION_EXPORT double GQLControlVersionNumber;
+
+//! Project version string for GQLControl.
+FOUNDATION_EXPORT const unsigned char GQLControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GQLControl/PublicHeader.h>
+
+
