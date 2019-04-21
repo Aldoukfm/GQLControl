d442af6fd2db5c7e1593080b46685a8361a6d06b
Aldo Fuentes <35090917+Aldoukfm@users.noreply.github.com>
2019-04-20 21:26:11 -0500

    QueryError localized description added.

--- GQLControl/QueryError.swift
+++ GQLControl/QueryError.swift
@@ -10,9 +10,25 @@ import Foundation
 
 public enum QueryError: String, Error {
     case nonImplemented
-    case operationTypeNotSupported
     case noData
     case decodingError
     case nonRequested
     case noURL
+}
+
+extension QueryError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .nonImplemented:
+            return "Query method \"execute\" non implemented"
+        case .noData:
+            return "Query returned no data"
+        case .decodingError:
+            return "Query could not decode result to specified object"
+        case .nonRequested:
+            return "Query did not fetch any data"
+        case .noURL:
+            return "Query could not initialize URL"
+        }
+    }
 }
